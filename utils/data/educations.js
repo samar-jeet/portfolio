c867253f8be5facfcc43039741bdc4c7b74b8ad4
samar-jeet <81514487+samar-jeet@users.noreply.github.com>
2024-05-07 01:41:09 +0530

    Update educations.js

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Bachelor in Technology",
+    duration: "2020 - 2024",
+    institution: "National Institute of Technologys, Allahabad, Prayagraj, India",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    title: "Intermediate Education",
+    // duration: "2018 - 2020",
+    institution: "Jawahar Navodaya Vidyalaya, Gajokhar, Varanasi, U.P, India",
   },
   {
     id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "High School Certificate",
+    // duration: "",
+    institution: "Jawahar Navodaya Vidyalaya, Gajokhar, Varanasi, U.P, India",
   }
 ]
