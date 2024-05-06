31132d7b9678adf492bc775a40e4f58a377fe6c5
samar-jeet <81514487+samar-jeet@users.noreply.github.com>
2024-05-07 02:02:18 +0530

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -5,7 +5,7 @@ export const contactsData = {
     github: 'https://github.com/samar-jeet',
     facebook: 'https://www.facebook.com/samarjeet.patel.16752/',
     linkedIn: 'https://www.linkedin.com/in/samarjeet-patel-86b58922a/',
-    twitter: 'https://twitter.com/rolex_392',
+    twitter: 'https://twitter.com/rolex_392'
     
     // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
     // devUsername: "said7388"
