208edf97c0619f0f221d3257b381cc976e6cc61b
samar-jeet <81514487+samar-jeet@users.noreply.github.com>
2024-06-10 13:29:05 +0530

    Update route.js

--- app/api/google/route.js
+++ app/api/google/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(request) {
   const reqBody = await request.json();
   const secret_key = "6LdZqdEpAAAAAP7eju4BO3kpYIpONZWRgBitl24H" ;
-    // process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY;
+    process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY;
 
   try {
     const url = `https://www.google.com/recaptcha/api/siteverify?secret=${secret_key}&response=${reqBody.token}`;
