1f4b47ec0707905e5c34bde91646c4e52f10a255
samar-jeet <81514487+samar-jeet@users.noreply.github.com>
2024-06-10 13:33:23 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,14 +4,14 @@ export const personalData = {
   designation: "Software Developer",
   description: "My name is Samarjeet Patel. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'samarjeetpatel394@gmail.com',
-  phone: '+8801608797655',
+  phone: '+9186011703**',
   address: 'Varanasi, U.P, India',
   github: 'https://github.com/samar-jeet',
   facebook: 'https://www.facebook.com/samarjeet.patel.16752/',
   linkedIn: 'https://www.linkedin.com/in/samarjeet-patel-86b58922a/',
   twitter: 'https://twitter.com/rolex_392',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
   leetcode: "https://leetcode.com/u/samarjeet_patel/",
-  devUsername: "said7388",
+  // devUsername: "said7388",
   resume: "https://drive.google.com/file/d/1zSt1onNeV8BE-LHEGZrjg9e8QK6QeMOh/view?usp=sharing"
 }
